d571b2dbde3d0d0c3eb38b9f3b5358e16c749a4d
ruhulaminrul <ruhul.ok@gmail.com>
2021-06-14 23:47:06 +0600

    service model

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function show(service $service)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(service $service)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, service $service)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(service $service)
+    {
+        //
+    }
+}
