62d5f6244b1203d46bd482d6df255655784d7aec
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-12-17 12:11:11 +0600

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -49,6 +49,10 @@
           Skillmaker is an e-learning and services platform established in 2020. We offer a wide variety of e-learning and services through e-learning courses and services based on current market trend particularly in the US-UK-EU-Canada-UAE markets. Align with the vision of the Bangladesh Government to become Digital Bangladesh, we take pride to be a part of this digital revolution by offering a great opportunities for the people, particularly for the segments of Students, Women, Underemployed People, Homemaker, Entrepreneurs & SMEs to be the active part of go digital and contribute heavily to their family, society and to the country at large by earning significant foreign currency.
         </p>
 
+        <p class="font-weight-bold font-italic text-center">
+        '' We are here for Empowering Societies in Digital means. ''
+        </p>
+
         <p>
           Skillmaker is here to provide scopes to the various segments of the people of the country to take the opportunities by building a wide variety of skillset and build their lifelong career. Our courses and services offer a wide variety of skills so that people can develop their proper skillset to compete in the current global market and earn independently. As Bangladesh is an emerging nation in digital market place we the Skillmaker promises to provide immense opportunities to the potential e-learners to fulfill their dream. Skillmaker is here to offer a great learning platform for the learners from where they can eventually see themselves as powerful e-services holders and earn significant foreign currency for them and for the country.revolution by offering a great opportunities for the people, particularly for the segments of Students, Women, Underemployed People, Homemaker, Entrepreneurs & SMEs to be the active part of go digital and contribute heavily to their family, society and to the country at large by earning significant foreign currency.
         </p>
