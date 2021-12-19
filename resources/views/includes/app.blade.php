a7525079b054b91df05c0191f2d700e80351ffd1
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-12-19 21:54:56 +0600

    whatsapp

--- resources/views/includes/app.blade.php
+++ resources/views/includes/app.blade.php
@@ -104,6 +104,45 @@
 
 
 @yield('js')
+
+
+<script>
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?69118';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+  "enabled":true,
+  "chatButtonSetting":{
+      "backgroundColor":"#4dc247",
+      "ctaText":"",
+      "borderRadius":"25",
+      "marginLeft":"0",
+      "marginBottom":"50",
+      "marginRight":"50",
+      "position":"right"
+  },
+  "brandSetting":{
+      "brandName":"Skill Maker",
+      "brandSubTitle":"Typically replies within a Hours",
+      "brandImg":"https://demos.abasas.tech/website/other/skillmaker/public/images/skillmaker.png",
+      "welcomeText":"Hi there!\nHow can I help you?",
+      "messageText":"Hello, I have a question about ",
+      "backgroundColor":"#0a5f54",
+      "ctaText":"Start Chat",
+      "borderRadius":"25",
+      "autoShow":false,
+      "phoneNumber":"+8801840000408"
+  }
+};
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+</script>
+
 </body>
 
 </html>
