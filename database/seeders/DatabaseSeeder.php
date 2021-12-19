60aa4ae5996dc03f28e6a619f8d7e45248fac18b
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-12-19 21:10:15 +0600

    chat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
-        $this->call(ChatSeeder::class);
-        $this->call(ChatMessageSeeder::class);
+        // $this->call(ChatSeeder::class);
+        // $this->call(ChatMessageSeeder::class);
 
 
 
