ab040d47951498f5c3cafc86fb73dbc2fad4ba65
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-12-30 12:02:56 +0600

    ssl initialize

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\OurTeamController;
 use App\Http\Controllers\SeminarController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\SslCommerzPaymentController;
 use App\Http\Controllers\SuccessStoriesController;
 use App\Http\Controllers\SupportController;
 use App\Http\Controllers\TrainingController;
@@ -128,3 +129,20 @@ Route::get('form', function () {
 Route::get('one-one', function () {
     return view('oneToOne');
 })->name('oneToOne');
+
+
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
