f7d0994faa6294f5142d0ed2b0f7822991f44390
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2022-01-01 05:16:40 +0600

    link

--- resources/views/includes/app.blade.php
+++ resources/views/includes/app.blade.php
@@ -49,12 +49,10 @@
         <i class="icofont-phone"></i> +1 5589 55488 55
         <i class="icofont-google-map"></i> A108 Adam Street, NY
       </div>
-      <div class="social-links">
-        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-        <a href="#" class="skype"><i class="icofont-skype"></i></a>
-        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      <div class="social-links"> 
+        <a href="{{App\Models\additionalSetting::find(9)->key }}" class="facebook"><i class="icofont-facebook"></i> </a>
+        <a href="{{App\Models\additionalSetting::find(10)->key   }}" class="instagram"><i class="icofont-instagram"></i></a> 
+        <a href="{{App\Models\additionalSetting::find(11)  }}" class="linkedin"><i class="icofont-linkedin"></i></i></a>
       </div>
     </div>
   </div>
